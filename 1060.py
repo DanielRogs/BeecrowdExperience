b49331f69e4893e35be73fb3907166f0ab68a62a
Daniel Rodrigues <90018065+DanielRogs@users.noreply.github.com>
2022-02-24 15:25:12 -0300

    1060 - Números Positivos

---
+++ 1060.py
@@ -0,0 +1,28 @@
+A = float(input())
+B = float(input())
+C = float(input())
+D = float(input())
+E = float(input())
+F = float(input())
+
+positive = 0
+
+if A > 0:
+    positive = positive + 1
+
+if B > 0:
+    positive = positive + 1
+
+if C > 0:
+    positive = positive + 1
+
+if D > 0:
+    positive = positive + 1
+
+if E > 0:
+    positive = positive + 1
+
+if F > 0:
+    positive = positive + 1
+
+print(positive, "valores positivos")
