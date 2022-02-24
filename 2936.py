705f4719de057185966574727daa7d71a4dc0bf2
Daniel Rodrigues <90018065+DanielRogs@users.noreply.github.com>
2022-02-24 15:46:48 -0300

    2936 - Quanta Mandioca?

---
+++ 2936.py
@@ -0,0 +1,21 @@
+C = 300
+T = 1500
+B = 600
+M = 1000
+I = 150
+D = 225
+
+PC = int(input())
+PT = int(input())
+PB = int(input())
+PM = int(input())
+PI = int(input())
+
+C = PC * C
+T = PT * T
+B = PB * B
+M = PM * M
+I = PI * I
+
+grama = C + T + B + M + I + D
+print(grama
